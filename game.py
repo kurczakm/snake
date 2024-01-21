30e1619010fc8f22778d1d4d05f18b9d2a6d28e9
kurczakm <212marcin@gmail.com>
2024-01-21 19:28:07 +0100

    Separate Game class

--- game.py
+++ game.py
@@ -1,98 +1,100 @@
 import asyncio
 import random
 
-BOARD_WIDTH = 10
-BOARD_HEIGHT = 10
-TICK = 0.4
-players = {}
-started = False
-food = []
-result = 'IN_PROGRESS'
 
+class Game:
+    def __init__(self):
+        self.board_width = 10
+        self.board_height = 10
+        self.tick = 0.4
+        self.players = {}
+        self.started = False
+        self.food = []
+        self.result = 'IN_PROGRESS'
 
-async def play(websocket):
-    from server import onTurnDone
+    async def play(self, websocket):
+        from server import onTurnDone
 
-    while True:
-        if started:
-            next_turn()
-            await onTurnDone(websocket)
-        await asyncio.sleep(TICK)
+        while True:
+            if self.started:
+                self.next_turn()
+                await onTurnDone(websocket, self)
+            await asyncio.sleep(self.tick)
 
+    def next_turn(self):
+        for playerName in self.players:
+            player = self.players[playerName]
+            player.last_direction = player.direction
+            food_eaten = player.move(self.food)
+            if food_eaten:
+                self.add_food()
 
-def next_turn():
-    for playerName in players:
-        player = players[playerName]
-        player.last_direction = player.direction
-        player.move()
+        self.check_collisions()
 
-    check_collisions()
+    def check_collisions(self):
+        for playerName in self.players:
+            player = self.players[playerName]
+            head = player.body_segments[0]
 
+            if head[0] < 0 or head[0] > self.board_width or head[1] < 0 or head[1] > self.board_height:
+                self.started = False
+                self.result = 'GAME_OVER'
+                return
 
-def check_collisions():
-    for playerName in players:
-        player = players[playerName]
-        head = player.body_segments[0]
+            for playerName2 in self.players:
+                player2 = self.players[playerName2]
 
-        for playerName2 in players:
-            player2 = players[playerName2]
+                for index, segment in enumerate(player2.body_segments):
+                    if playerName2 == playerName and index == 0:
+                        continue
 
-            for index, segment in enumerate(player2.body_segments):
-                if playerName2 == playerName and index == 0:
-                    continue
+                    if segment == head:
+                        self.started = False
+                        self.result = 'GAME_OVER'
+                        return
 
-                if segment == head:
-                    global started, result
-                    started = False
-                    result = 'GAME_OVER'
+    def get_players_info(self):
+        players_info = []
+        for playerName in self.players:
+            player = self.players[playerName]
+            players_info.append({
+                'name': player.name,
+                'color': player.color,
+                'bodySegments': player.body_segments
+            })
 
+        return {
+            'type': 'status',
+            'food': self.food,
+            'players': players_info,
+            'gameResult': self.result
+        }
 
-def get_players_info():
-    players_info = []
-    for playerName in players:
-        player = players[playerName]
-        players_info.append({
-            'name': player.name,
-            'color': player.color,
-            'bodySegments': player.body_segments
-        })
+    def add_food(self):
+        x_candidates = list(range(0, self.board_width))
+        y_candidates = list(range(0, self.board_height))
 
-    return {
-        'type': 'status',
-        'food': food,
-        'players': players_info,
-        'gameResult': result
-    }
+        for playerName in self.players:
+            player = self.players[playerName]
+            for segment in player.body_segments:
+                try:
+                    x_candidates.remove(segment[0])
+                except ValueError:
+                    pass
+                try:
+                    y_candidates.remove(segment[1])
+                except ValueError:
+                    pass
 
+        x = random.choice(x_candidates)
+        y = random.choice(y_candidates)
 
-def add_food():
-    x_candidates = list(range(0, BOARD_WIDTH))
-    y_candidates = list(range(0, BOARD_HEIGHT))
+        self.food.append([x, y])
 
-    for playerName in players:
-        player = players[playerName]
-        for segment in player.body_segments:
-            try:
-                x_candidates.remove(segment[0])
-            except ValueError:
-                pass
-            try:
-                y_candidates.remove(segment[1])
-            except ValueError:
-                pass
-
-    x = random.choice(x_candidates)
-    y = random.choice(y_candidates)
-
-    food.append([x, y])
-
-
-def start():
-    global food
-    food = []
-    add_food()
-    global started
-    started = True
+    def start(self):
+        self.food = []
+        self.add_food()
+        self.started = True
 
 
 class Player:
@@ -103,21 +105,17 @@ class Player:
         self.direction = direction
         self.last_direction = direction
 
-    def move(self):
+    def move(self, food):
         x, y = self.body_segments[0]
         match self.direction:
             case 'left':
-                if x > 0:
-                    x = x - 1
+                x = x - 1
             case 'right':
-                if x < BOARD_WIDTH - 1:
-                    x = x + 1
+                x = x + 1
             case 'up':
-                if y > 0:
-                    y = y - 1
+                y = y - 1
             case 'down':
-                if y < BOARD_HEIGHT - 1:
-                    y = y + 1
+                y = y + 1
         self.body_segments.insert(0, [x, y])
 
         food_eaten = -1
@@ -128,9 +126,10 @@ class Player:
 
         if food_eaten == -1:
             del self.body_segments[-1]
+            return False
         else:
             del food[food_eaten]
-            add_food()
+            return True
 
     def setDirection(self, direction):
         if direction == 'up' and self.last_direction == 'down':
--- server.py
+++ server.py
@@ -2,47 +2,54 @@ import asyncio
 import websockets
 import json
 
-from game import BOARD_WIDTH, BOARD_HEIGHT, Player, players, play, get_players_info, start, result
+from game import Player, Game
 
 
-async def onTurnDone(websocket):
-    await send_positions(websocket)
+async def onTurnDone(websocket, game):
+    await send_positions(websocket, game)
 
 
-async def send_positions(websocket):
-    await websocket.send(json.dumps(get_players_info()))
+async def send_positions(websocket, game):
+    await websocket.send(json.dumps(game.get_players_info()))
 
 
-async def send_game_info(websocket):
+async def send_game_info(websocket, game):
     game_info = {
         'type': 'game_info',
-        'width': BOARD_WIDTH,
-        'height': BOARD_HEIGHT
+        'width': game.board_width,
+        'height': game.board_height
     }
     await websocket.send(json.dumps(game_info))
 
 
 async def handler(websocket):
-    play_task = asyncio.create_task(play(websocket))
-
-    async for message in websocket:
-        await handle_message(websocket, message)
-
-    await play_task
+    message = await websocket.recv()
+    await handle_init_message(websocket, message)
 
 
-async def handle_message(websocket, message):
+async def handle_init_message(websocket, init_message):
+    data = json.loads(init_message)
+    if data['action'] == 'start':
+        game = Game()
+        play_task = asyncio.create_task(game.play(websocket))
+        game.start()
+        await send_game_info(websocket, game)
+        player1 = Player(data['player']['name'], data['player']['color'], [[1, 1]], 'down')
+        game.players[player1.name] = player1
+
+        async for message in websocket:
+            await handle_message(game, websocket, message)
+
+        await play_task
+
+
+async def handle_message(game, websocket, message):
     data = json.loads(message)
     print(data)
 
     match data['action']:
-        case 'start':
-            start()
-            await send_game_info(websocket)
-            player1 = Player(data['player']['name'], data['player']['color'], [[1, 1]], 'down')
-            players[player1.name] = player1
         case 'move':
-            player = players[data['playerName']]
+            player = game.players[data['playerName']]
             direction = data['direction']
             player.setDirection(direction)
 
